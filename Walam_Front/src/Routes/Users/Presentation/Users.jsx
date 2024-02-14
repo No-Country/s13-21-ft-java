7c3c27343bc9886fd0bb2cd77e445cd85e2fa148
Jorge Nahuel Beja Rosa <jorgebejarosa@gmail.com>
2024-02-14 12:26:35 -0300

    Add: creación proyecto Front con React

---
+++ Walam_Front/src/Routes/Users/Presentation/Users.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UsersLayout from './UsersLayout'
+import UsersInfo from './UsersInfo'
+
+const Users = () => {
+  return (
+    <UsersLayout>
+      <UsersInfo />
+    </UsersLayout>
+  )
+}
+
+export default Users
---
+++ Walam_Front/src/components/index.js
@@ -0,0 +1,4 @@
+import Footer from './Footer/Footer'
+import NavBar from './Navbar/Navbar'
+
+export { Footer, NavBar }
