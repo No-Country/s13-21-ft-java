b86a9b98c790e76b5b4abca6bfc5abd835fff225
patinoricar <patinoricar@gmail.com>
2024-02-19 23:06:41 -0500

    Preloader OnBoarding

--- Walam_Front/src/Routes/OnBoarding/Presentation/OnBoarding.jsx
+++ Walam_Front/src/Routes/OnBoarding/Presentation/OnBoarding.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import OnBoardingLayout from './OnBoardingLayout'
 import OnBoardingInfo from './OnBoardingInfo'
+import Loader from './Loader';
 
 const OnBoarding = () => {
+
+  const [screenLoading, setScreenLoading] = useState(false);
+
+  useEffect(() => {
+    setScreenLoading(true);
+    setTimeout(() => {
+      setScreenLoading(false);
+    }, 2500);
+  }, []);
+
   return (
-    <OnBoardingLayout>
-      <OnBoardingInfo />
-    </OnBoardingLayout>
+    <>
+      {screenLoading ? (
+        <Loader />
+      ) : (
+        <OnBoardingLayout>
+          <OnBoardingInfo />
+        </OnBoardingLayout>
+      )}
+    </>
   )
 }
 
