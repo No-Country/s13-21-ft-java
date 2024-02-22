86aec6656bea39c5fece78f9edc7dc68077da71d
Matias Nicolas Acevedo <matias.nicolas.acevedo@gmail.com>
2024-02-22 20:20:43 -0300

    getById y getAll todo ok | funcionando con token

--- Walam/src/main/java/org/nocountry/walam/main/service/impl/UserService.java
+++ Walam/src/main/java/org/nocountry/walam/main/service/impl/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -18,5 +19,13 @@ public class UserService {
     }
     public List<User> getUser(){
         return userRepository.findAll();
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Integer id) {
+        return userRepository.findById(id);
     }
 }
