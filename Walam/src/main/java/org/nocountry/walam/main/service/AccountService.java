12909d503ed998e45bfe77b492e81d8d5148e222
Juan Ortega <91577323+Full-Juan-Ortega@users.noreply.github.com>
2024-02-22 14:06:52 -0300

    fix dtoExample and add cors config

--- Walam/src/main/java/org/nocountry/walam/main/service/AccountService.java
+++ Walam/src/main/java/org/nocountry/walam/main/service/AccountService.java
@@ -2,25 +2,22 @@ package org.nocountry.walam.main.service;
 
 import org.nocountry.walam.main.model.dto.AccountDTO;
 import org.nocountry.walam.main.model.entity.Account;
-import org.nocountry.walam.main.model.mapper.AccountMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public interface AccountService {
+
+    Account saveAccount(Account account);
+    void deleteAccountById(int id);
+    List<Account> getAllAccounts();
+    Account getAccountById(int id);
+
+    /* MAPPER EXAMPLE */
+    AccountDTO mapAccountToDTO(Account account);
+    Account mapDTOToAccount(AccountDTO accountDTO);
 
 
-@Service
-public class AccountService {
 
-/*    public AccountDTO mapAccountToDTO(Account account)
-    public Account mapDTOToAccount(AccountDTO accountDTO)*/
-
-    public AccountDTO mapAccountToDTO(Account account) {
-        return AccountMapper.INSTANCE.accountToAccountDTO(account);
-    }
-
-    public Account mapDTOToAccount(AccountDTO accountDTO) {
-        return AccountMapper.INSTANCE.accountDTOToAccount(accountDTO);
-    }
-
-
+    // Otras operaciones si es necesario.
 
 }
