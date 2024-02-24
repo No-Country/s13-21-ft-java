0a37ef71d2bb8a3336500813db3af6362fe41c64
Joshua Frías <metatron1495@gmail.com>
2024-02-23 22:47:11 -0600

    Add: Concluyo servicio e implementación para entidad paises

--- Walam/src/main/java/org/nocountry/walam/main/model/repository/CountryRepository.java
+++ Walam/src/main/java/org/nocountry/walam/main/model/repository/CountryRepository.java
@@ -1,9 +1,9 @@
 package org.nocountry.walam.main.model.repository;
 
 import org.nocountry.walam.main.model.entity.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CountryRepository extends JpaRepository<Country, Integer> {
+public interface CountryRepository extends CrudRepository<Country, Integer> {
 }
