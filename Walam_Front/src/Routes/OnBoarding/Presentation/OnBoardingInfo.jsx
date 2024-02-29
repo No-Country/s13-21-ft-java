e7f4afe28e934294204181f864529383de0ab278
patinoricar <patinoricar@gmail.com>
2024-02-28 21:29:03 -0500

    Responsive Screen Changes - HomePage

--- Walam_Front/src/Routes/OnBoarding/Presentation/OnBoardingInfo.jsx
+++ Walam_Front/src/Routes/OnBoarding/Presentation/OnBoardingInfo.jsx
@@ -18,8 +18,8 @@ const OnBoardingInfo = () => {
       <li className='p-1 font-normal'>
         <a href='#' className=''>Servicios</a>
       </li>
-      <li className='p-1 font-normal'>
-        <a href='#' className=''>Tarjeta</a>
+      <li className="p-1 font-normal">
+        <a href="#" className="">Tarjeta</a>
       </li>
       <li className='p-1 font-normal'>
         <a href='#' className=''>Prestamos sustentables</a>
@@ -27,25 +27,10 @@ const OnBoardingInfo = () => {
     </ul>
   )
 
-  const converted = {
-    background: [
-      '#33c751',
-      '-webkit-linear-gradient(to right, #33c751, #001f04)',
-      'linear-gradient(to right, #33c751, #001f04)'
-    ],
-    WebkitBackgroundClip: 'text',
-    backgroundClip: 'text',
-    color: 'transparent'
-  }
-
   return (
     <div
-      className='bg-[#222121] h-full w-full pt-12'
-      style={{
-        backgroundImage: 'url("/img/Group1x_75.png"), url("/img/Group0x_75.png")',
-        backgroundPosition: '0px 0px, -146px -115px',
-        backgroundRepeat: 'no-repeat'
-      }}
+      className='bg-[#222121] h-full w-full pt-12 lg:bg-OnBoarding-bgImage lg:bg-OnBoarding-bgPosition lg:bg-no-repeat'
+      
     >
 
       <header className='sticky top-0 bg-[#1E1E1E99] shadow-md backdrop-blur'>
@@ -64,7 +49,7 @@ const OnBoardingInfo = () => {
                 className='ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden'
                 onClick={() => setOpenNav(!openNav)}
               >
-                {openNav ? <IoIosCloseCircle /> : <IoIosApps />}
+                {openNav ? <IoIosCloseCircle className="w-[25px] h-[25px]" /> : <IoIosApps className="w-[25px] h-[25px]" />}
               </button>
             </div>
           </div>
@@ -80,18 +65,18 @@ const OnBoardingInfo = () => {
 
       <main className='w-full flex flex-col justify-center items-center'>
 
-        <div className='pl-[25%] pt-[95px] pb-[200px]'>
+        <div className="h-[512px] pt-[95px] pb-[200px] pl-[22%] flex flex-col">
           <p className='text-5xl'>La primer</p>
           <p className="py-3 text-8xl bg-gradient-to-r from-[#B2FA5B] to-[#3BC53F] inline-block text-transparent bg-clip-text">Green FinTech </p>
           <p className='text-5xl'>de Latinoamérica</p>
         </div>
 
-        <div className='py-16 pl-16 text-center bg-[#3E3E3Eb8] w-full border-b-2 border-black shadow-inner shadow-md'>
-          <p className='text-left text-4xl pb-12'>Servicios Latam</p>
-          <div className='w-full flex flex-wrap justify-between'>
-            <div className='w-[350px] text-left'>
-              <h2 className='text-2xl pb-2'>Envía Dinero</h2>
-              <p>Fácil, rápida y segura que te permite enviar dinero a cualquier partedel continente sin importar si tiene o no una cuenta bancaria.</p>
+        <div className='py-16 pl-[60px] text-center bg-[#3E3E3Eb8] w-full border-b-2 border-black shadow-inner shadow-md'>
+          <p className="text-left text-4xl">Servicios Latam</p>
+          <div className="w-full flex flex-wrap justify-center items-center">
+            <div className="w-[350px] pr-[20px] pt-10 text-left">
+              <h2 className="text-2xl pb-2">Envía Dinero</h2>
+              <p>Fácil, rápida y segura que te permite enviar dinero a cualquier parte del continente sin importar si tiene o no una cuenta bancaria.</p>
               <h2 className='text-2xl pb-2 pt-8'>Transferencias</h2>
               <p>Realiza transferencias gratis y al instante: a contactos, cuentas bancarias o billeteras virtuales.</p>
               <h2 className='text-2xl pb-2 pt-8'>Extracciones</h2>
@@ -99,26 +84,26 @@ const OnBoardingInfo = () => {
               <h2 className='text-2xl pb-2 pt-8'>Cambio de Divisas</h2>
               <p>"Viaja sin fronteras: cambia divisas al instante”</p>
             </div>
-            <img src='/img/Group2.png' alt='Lugares de Latinoamérica' className='w-2/3' />
+            <img src="/img/Group2.png" alt="Lugares de Latinoamérica" className="size-5/6 pt-10 lg:size-2/3" />
           </div>
         </div>
 
-        <div className='p-[100px] w-full text-right border-b-2 border-black shadow-inner shadow-md flex flex-wrap justify-evenly'>
-          <div className='w-2/5 flex flex-col justify-around items-end'>
-            <p className='text-4xl'>Solicita tu tarjeta Prepaga Virtual sin Costo y sin Burocracias</p>
-            <img src='/img/Top Estate Agent.png' alt='Usuarios EcoPay' className='w-[280px]' />
+        <div className='py-[80px] px-[20px] w-full text-right border-b-2 border-black shadow-inner shadow-md flex flex-wrap justify-center'>
+          <div className="w-[380px] flex flex-col justify-around items-end py-2 pr-8">
+            <p className="text-4xl">Solicita tu tarjeta Prepaga Virtual sin Costo y sin Burocracias</p>
+            <img src="/img/Top Estate Agent.png" alt="Usuarios EcoPay" className="w-[280px] pt-2" />
           </div>
-          <img src='/img/Card.png' alt='Targeta EcoPay' />
+          <img src="/img/Card.png" alt="Targeta EcoPay" className="w-[380px] py-2" />
         </div>
 
-        <div className='p-[60px] w-full border-b-2 border-black shadow-inner shadow-md flex flex-wrap justify-evenly'>
-          <div className='w-2/5 flex flex-col justify-around items-end text-right'>
-            <img src='/img/Blog.png' alt='Usuarios EcoPay' className='w-[100px] pb-3' />
-            <img src='/img/Blog2.png' alt='Usuarios EcoPay' className='w-[260px]' />
+        <div className='py-[40px] px-[20px] w-full border-b-2 border-black shadow-inner shadow-md flex flex-wrap justify-center'>
+          <div className="w-[320px] pr-[20px] py-2 flex flex-col justify-around items-end text-right">
+            <img src="/img/Blog.png" alt="Usuarios EcoPay" className="w-[100px] pb-3" />
+            <img src="/img/Blog2.png" alt="Usuarios EcoPay" className="w-[260px]" />
           </div>
-          <div className='w-2/5 flex flex-col justify-around items-start text-left'>
-            <img src='/img/Blog3.png' alt='Usuarios EcoPay' className='w-[300px]' />
-            <p className='text-4xl w-[250px]'>Prestamos para emprendimientos sustentables</p>
+          <div className="w-[320px] pl-[20px] py-2 flex flex-col justify-around items-start text-left">
+            <img src="/img/Blog3.png" alt="Usuarios EcoPay" className="w-[300px]" />
+            <p className="text-4xl w-[300px] pt-2">Prestamos para emprendimientos sustentables</p>
           </div>
         </div>
 
@@ -134,26 +119,30 @@ const OnBoardingInfo = () => {
           </div>
         </div>
 
-        <div className="w-full flex flex-col items-center"
-        style={{backgroundImage: `url("/img/GroupFooter.png")`,
-        backgroundPosition: 'right 0px',
-        backgroundRepeat: 'no-repeat'}}>
-        <div className="flex flex-wrap justify-around w-full bg-[#3E3E3Eb8] shadow-inner shadow-md">
+        <div className="w-full flex flex-col items-center lg:bg-OnBoardingFooter-bgImage lg:bg-OnBoardingFooter-bgPosition lg:bg-no-repeat">
+        <div className="flex flex-wrap justify-around w-full bg-[#3E3E3Eb8] shadow-md">
 
-          <div className="flex flex-col justify-between items-center py-6">
-            <div className="flex flex-wrap justify-between">
+          <div className="flex flex-col justify-center items-center pb-6">
+            <div className="flex flex-wrap justify-center">
 
-              <div className="flex flex-col px-6 justify-center items-start">
+              <div className="flex flex-col px-6 pt-6 justify-center items-start">
                 <p className="p-2"><Link to='/UsersDataForm' >UsersDataForm</Link></p>
                 <p className="p-2"><Link to='/Login' >Login</Link></p>
                 <p className="p-2"><Link to='/Register' >Register</Link></p>
                 <p className="p-2"><Link to='/ForgotPassword' >ForgotPassword</Link></p>
               </div>
-              <div className="flex flex-col px-6 justify-center items-start">
+              <div className="flex flex-col px-6 pt-6 justify-center items-start">
                 <p className="p-2"><Link to='/DashboardUser' >DashboardUser</Link></p>
                 <p className="p-2"><Link to='/Deposit' >Deposit</Link></p>
                 <p className="p-2"><Link to='/Extract' >Extract</Link></p>
                 <p className="p-2"><Link to='/VirtualCashier' >VirtualCashier</Link></p>
+              </div>
+              <div className="flex flex-col px-6 pt-6 justify-center items-start">
+                <p className="p-2"><Link to='/Transfer' >Transfer</Link></p>
+                <p className="p-2"><Link to='/ConfirmTransfer' >ConfirmTransfer</Link></p>
+                <p className="p-2"><Link to='/MovementsHistory' >MovementsHistory</Link></p>
+                <p className="p-2"><Link to='/HistoryDetails' >HistoryDetails</Link></p>
+                <p className="p-2"><Link to='/CVUUser' >CVUUser</Link></p>
               </div>
             </div>
             <div className="flex flex-col justify-between items-center w-[400px] pt-4">
@@ -180,12 +169,12 @@ const OnBoardingInfo = () => {
         </div>
 
         <div className="p-4">
-          <p>* Copyright © 2023-2024 Grupo Java. Todos los derechos reservados. EcoPay .   Hecho en Argentina whit love jajajaja.</p>
+          <p>* Copyright © 2023-2024 Grupo Java. Todos los derechos reservados. EcoPay.</p>
         </div>
 
         </div>
 
-     </footer>
+      </footer>
 
     </div>
   )
