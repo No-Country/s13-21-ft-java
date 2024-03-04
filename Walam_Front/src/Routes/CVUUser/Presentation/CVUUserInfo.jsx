edf2576289a0999823a9b44ef89278748e7b2bff
Jorge Nahuel Beja Rosa <jorgebejarosa@gmail.com>
2024-03-04 17:09:10 -0300

    Conexión rutas Backend Front

--- Walam_Front/src/Routes/CVUUser/Presentation/CVUUserInfo.jsx
+++ Walam_Front/src/Routes/CVUUser/Presentation/CVUUserInfo.jsx
@@ -1,7 +1,30 @@
 import { Link } from 'react-router-dom'
 import { FaArrowLeft } from 'react-icons/fa'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function CVUUserInfo () {
+  const [user, setUser] = useState()
+  const [user2, setUser2] = useState()
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const username = localStorage.getItem('username')
+        const token = localStorage.getItem('token')
+        axios.defaults.headers.common.Authorization = `Bearer ${token}`
+        const response = await axios.get('https://s13-21-ft-java.onrender.com/api/v1/users')
+        setUser(response.data.account.cvu)
+        setUser2(response.data.username)
+        console.log(response.data)
+      } catch (error) {
+        console.error('Error al guardar usuario:', error)
+      }
+    }
+
+    fetchUser()
+  }, [])
+
   return (
     <div className='flex flex-col px-8'>
       <div className='flex items-center gap-3'>
@@ -16,12 +39,12 @@ export default function CVUUserInfo () {
         <div className='shadow-inner rounded-2xl mt-6 p-6 flex flex-col items-center border-2 xl:border xl:rounded-xl bg-zinc-800 xl:shadow-md xl:outline-1 xl:border-neutral-700 '>
           <h2 className='font-bold pb-3 text-xl'>CVU</h2>
           <hr className='border-1 w-[300px]' />
-          <p className='py-3'>123456789101</p>
+          <p className='py-3'>{user} </p>
           <hr className='border-1 w-[300px]' />
           <div />
           <h2 className='font-bold pb-3 pt-4 text-xl'>Alias</h2>
           <hr className=' pt-3 border-1 w-[300px]' />
-          <p>pedrit@478</p>
+          <p>{user2}</p>
         </div>
       </div>
     </div>
--- Walam_Front/src/Routes/UsersDataForm/Presentation/UsersDataFormInfo.jsx
+++ Walam_Front/src/Routes/UsersDataForm/Presentation/UsersDataFormInfo.jsx
@@ -3,112 +3,107 @@ import { Formik, Form } from 'formik'
 import * as Yup from 'yup'
 import { FormButton, FormInput } from '../../../components'
 import { FaArrowLeft } from 'react-icons/fa'
-import { GoPencil } from "react-icons/go"
-import { useDeleteUserMutation, useUpdateUserMutation, useGetLoggedInUserQuery } from '../../../api/apiSlice'
+import { GoPencil } from 'react-icons/go'
+import axios from 'axios'
+import { useState } from 'react'
 
 export default function UsersDataFormInfo () {
   const navigate = useNavigate()
-  const [deleteUser] = useDeleteUserMutation()
-  const [updateUser] = useUpdateUserMutation()
-  const { data: loggedInUser } = useGetLoggedInUserQuery()
-  // Conexion a la API (onsubmit/onreset)
+  const [formValues, setFormValues] = useState({
+    firstName: '',
+    lastName: '',
+    noIdentidad: '',
+    birthday: '',
+    phone: '',
+    country: ''
+  })
 
   // Validaciones
   const validationSchema = Yup.object().shape({
-    name: Yup.string().min(3, 'Mínimo 3 caractares').max(20, 'Máximo 20 caracteres').required('Nombre requerido'),
-    lastname: Yup.string().min(3, 'Mínimo 3 caractares').max(20, 'Máximo 20 caracteres').required('Apellido requerido'),
-    birth: Yup.date().required('Fecha requerida'),
+    firstName: Yup.string().min(3, 'Mínimo 3 caractares').max(20, 'Máximo 20 caracteres').required('Nombre requerido'),
+    lastName: Yup.string().min(3, 'Mínimo 3 caractares').max(20, 'Máximo 20 caracteres').required('Apellido requerido'),
+    birthday: Yup.date().required('Fecha requerida'),
     phone: Yup.number().min(6, 'Mínimo 6 caractares').required('Teléfono requerido'),
     country: Yup.string().min(3, 'Mínimo 3 caractares').max(20, 'Máximo 20 caracteres').required('País requerido'),
-    docNumber: Yup.number().min(6, 'Mínimo 6 caractares').required('Número de documento requerido')
+    noIdentidad: Yup.number().min(6, 'Mínimo 6 caractares').required('Número de documento requerido')
   })
 
-  const initialValues = {
-    name: loggedInUser?.name || '',
-    lastname: loggedInUser?.lastname || '',
-    birth: loggedInUser?.birth || '',
-    phone: loggedInUser?.phone || '',
-    country: loggedInUser?.country || '',
-    docNumber: loggedInUser?.docNumber || ''
-  }
-
-  // const handleSubmit = (values) => {
-  //   const { name, lastname, birth, phone, country, residence, docNumber } = values
-
-  //   console.log(name, lastname, birth, phone, country, residence, docNumber)
-
-  //   createUser({ name, lastname, birth, phone, country, residence, docNumber })
-  //   navigate('/DashboardUser')
-  // }
-
-  const handleSubmit = (values) => {
-    const updatedUserData = {
-      ...loggedInUser,
-      ...values
+  const handleSubmit = async (values) => {
+    const { firstName, lastName, noIdentidad, phone, birthday, country } = values
+    const userData = {
+      firstName,
+      lastName,
+      noIdentidad,
+      birthday,
+      phone,
+      country
     }
 
-    updateUser({ id: loggedInUser.id, userData: updatedUserData })
-      .unwrap()
-      .then(() => navigate('/DashboardUser'))
-      .catch((error) => console.error('Error al actualizar usuario:', error))
+    try {
+      setFormValues(values)
+      const token = localStorage.getItem('token')
+      axios.defaults.headers.common.Authorization = `Bearer ${token}`
+      await axios.put('https://s13-21-ft-java.onrender.com/api/v1/user-update', userData)
+      navigate('/DashboardUser')
+      console.log(userData)
+    } catch (error) {
+      console.error('Error al guardar usuario:', error)
+    }
   }
 
   const handleDelete = () => {
-    deleteUser(loggedInUser.id)
-      .unwrap()
-      .then(() => navigate('/DashboardUser'))
-      .catch((error) => console.error('Error al eliminar usuario:', error))
+    navigate('/DashboardUser')
   }
 
-  return (    
-      <section className='text-white rounded-xl min-w-[350px] w-4/5 xl:max-h-[550px] py-2'> 
-          <div className='flex items-center gap-3 self-start p-2'>
-            <Link to='/DashboardUser'><FaArrowLeft /></Link>
-            <h2 className='text-xl font-semibold'>Perfil</h2>            
-          </div>
-          <div className='w-full flex flex-col h-full overflow-y-auto'>
-            <Formik
-              initialValues={initialValues}
-              onSubmit={handleSubmit}
-              validationSchema={validationSchema}
-            >
-              {({ errors, values }) => (
-                <Form className='rounded flex flex-col justify-between px-6'>
-                  <div>
-                    <div className='flex items-center gap-2'>
-                      <FormInput name='Nombres' type='text' placeholder='Juan Martin' errors={errors} id='name' value={values.name} />
-                      <GoPencil className='bg-gradient-to-b from-[#3BC53F] to-[#B2FA5B] h-8 w-8 p-1 rounded-xl'/>
-                    </div>
-                    <div className='flex items-center gap-2'>
-                      <FormInput name='Apellidos' type='text' placeholder='López López' errors={errors} id='lastname' value={values.lastname} />
-                      <GoPencil className='bg-gradient-to-b from-[#3BC53F] to-[#B2FA5B] h-8 w-8 p-1 rounded-xl'/>
-                    </div>
-                    <div className='flex items-center gap-2'>
-                      <FormInput name='Número de Documento de Identidad' type='tel' placeholder='0000000000' errors={errors} id='docNumber' value={values.docNumber} />
-                      <GoPencil className='bg-gradient-to-b from-[#3BC53F] to-[#B2FA5B] h-8 w-8 p-1 rounded-xl'/>
-                    </div>
-                    <div className='flex items-center gap-2'>
-                      <FormInput name='Teléfono' type='tel' placeholder='(0000)00000000' errors={errors} id='phone' value={values.phone} />
-                      <GoPencil className='bg-gradient-to-b from-[#3BC53F] to-[#B2FA5B] h-8 w-8 p-1 rounded-xl'/>
-                    </div>
-                    <div className='flex items-center gap-2'>
-                      <FormInput name='Fecha de Nacimiento' type='date' placeholder='00 ENE 0000' errors={errors} id='birth' value={values.birth} />
-                      <GoPencil className='bg-gradient-to-b from-[#3BC53F] to-[#B2FA5B] h-8 w-8 p-1 rounded-xl'/>
-                    </div>
-                    <div className='flex items-center gap-2'>
-                      <FormInput name='País' type='text' placeholder='Colombia' errors={errors} id='country' value={values.country} />
-                      <GoPencil className='bg-gradient-to-b from-[#3BC53F] to-[#B2FA5B] h-8 w-8 p-1 rounded-xl'/>
-                    </div>                    
-                  </div>
-                  <div className='flex flex-col items-center pb-3'>
-                    <FormButton text='Guardar Datos' hover='hover:bg-primarygray' />
-                    <button onClick={handleDelete} className='m-3 text-center text-sm font-semibold text-red-600 hover:bg-red-600 hover:text-white rounded-lg py-2 px-4 w-full mb-16'>Eliminar Cuenta</button>
-                  </div>
-                </Form>
-              )}
-            </Formik>            
-          </div>
-        
-      </section> 
+  return (
+    <section className='text-white rounded-xl min-w-[350px] w-4/5 xl:max-h-[550px] py-2'>
+      <div className='flex items-center gap-3 self-start p-2'>
+        <Link to='/DashboardUser'><FaArrowLeft /></Link>
+        <h2 className='text-xl font-semibold'>Perfil</h2>
+      </div>
+      <div className='w-full flex flex-col h-full overflow-y-auto'>
+        <Formik
+          initialValues={formValues}
+          onSubmit={handleSubmit}
+          validationSchema={validationSchema}
+        >
+          {({ errors, values }) => (
+            <Form className='rounded flex flex-col justify-between px-6'>
+              <div>
+                <div className='flex items-center gap-2'>
+                  <FormInput name='Nombres' type='text' placeholder='Juan Martin' errors={errors} id='firstName' value={values.firstName} />
+                  <GoPencil className='bg-gradient-to-b from-[#3BC53F] to-[#B2FA5B] h-8 w-8 p-1 rounded-xl' />
+                </div>
+                <div className='flex items-center gap-2'>
+                  <FormInput name='Apellidos' type='text' placeholder='López López' errors={errors} id='lastName' value={values.lastName} />
+                  <GoPencil className='bg-gradient-to-b from-[#3BC53F] to-[#B2FA5B] h-8 w-8 p-1 rounded-xl' />
+                </div>
+                <div className='flex items-center gap-2'>
+                  <FormInput name='Número de Documento de Identidad' type='tel' placeholder='0000000000' errors={errors} id='noIdentidad' value={values.noIdentidad} />
+                  <GoPencil className='bg-gradient-to-b from-[#3BC53F] to-[#B2FA5B] h-8 w-8 p-1 rounded-xl' />
+                </div>
+                <div className='flex items-center gap-2'>
+                  <FormInput name='Teléfono' type='tel' placeholder='(0000)00000000' errors={errors} id='phone' value={values.phone} />
+                  <GoPencil className='bg-gradient-to-b from-[#3BC53F] to-[#B2FA5B] h-8 w-8 p-1 rounded-xl' />
+                </div>
+                <div className='flex items-center gap-2'>
+                  <FormInput name='Fecha de Nacimiento' type='date' placeholder='00 ENE 0000' errors={errors} id='birthday' value={values.birthday} />
+                  <GoPencil className='bg-gradient-to-b from-[#3BC53F] to-[#B2FA5B] h-8 w-8 p-1 rounded-xl' />
+                </div>
+                <div className='flex items-center gap-2'>
+                  <FormInput name='País' type='text' placeholder='Colombia' errors={errors} id='country' value={values.country} />
+                  <GoPencil className='bg-gradient-to-b from-[#3BC53F] to-[#B2FA5B] h-8 w-8 p-1 rounded-xl' />
+                </div>
+              </div>
+              <div className='flex flex-col items-center pb-3'>
+                <FormButton text='Guardar Datos' hover='hover:bg-primarygray' />
+                <button onClick={handleDelete} className='m-3 text-center text-sm font-semibold text-red-600 hover:bg-red-600 hover:text-white rounded-lg py-2 px-4 w-full mb-16'>Eliminar Cuenta</button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+
+    </section>
   )
 }
