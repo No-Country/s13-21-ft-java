c76db42e4c0f066a3583349a0ffd2338e417ef0d
Diana Castaño <dianaczapata@gmail.com>
2024-03-06 13:19:24 -0500

    update scrolling

--- Walam_Front/src/Routes/ForeignExchange/Presentation/ForeignExchangeInfo.jsx
+++ Walam_Front/src/Routes/ForeignExchange/Presentation/ForeignExchangeInfo.jsx
@@ -1,5 +1,3 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
 import { DisplayDate, ForeignInfo } from '../../../components'
 import { Link } from 'react-router-dom'
 import { FaArrowLeft } from 'react-icons/fa'
@@ -50,29 +48,15 @@ export default function ForeignExchangeInfo () {
     }
   ]
 
-  // useEffect(() => {
-  //   const fetchExchangeRate = async () => {
-  //     try {
-  //       const response = await axios.get('https://dolarapi.com/v1/cotizaciones')
-  //       setExchangeRate(response.data)
-  //       setDateString(response.data[0].fechaActualizacion)
-  //     } catch (error) {
-  //       console.error('Error fetching exchange rate:', error)
-  //     }
-  //   }
-
-  //   fetchExchangeRate()
-  // }, [])
-
   return (
-    <div>
-      <div className='flex items-center gap-2 xl:gap-0'>
+    <div className='overflow-auto'>
+      <div className='flex items-center gap-2 xl:gap-0 '>
         <Link to='/DashBoardUser' className='pl-4 self-start pt-2'>            <FaArrowLeft />
         </Link>
         <h2 className='text-center pb-3 font-bold text-2xl'>Tipo de cambio de moneda a pesos argentinos:</h2>
       </div>
       <DisplayDate dateString={dateString} />
-      <div className='w-full flex flex-col items-center'>
+      <div className='w-full flex flex-col items-center  '>
         {exchangeRate && exchangeRate.map((data, key) => {
           return <ForeignInfo moneda={data.moneda} venta={data.venta} compra={data.compra} key={key} />
         })}
